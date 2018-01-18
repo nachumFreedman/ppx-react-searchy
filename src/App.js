3f59069ba871b5975cd35f4534e84d38fe0cb0d3
nachum freedman <nochum09@gmail.com>
2018-01-18 05:20:13 +0200

    made a google logo with some style on search + added im feeling lucky button that does nothing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Query from './Query';
@@ -23,13 +22,7 @@ class App extends Component {
     
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         {
           results.length ? (
             <Results results={results} onReset={this.reset}/>
--- src/Query.js
+++ src/Query.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import './Query.css';
 
-const imgSrc = 'http://www.azquotes.com/picture-quotes/quote-the-first-principle-is-that-you-must-not-fool-yourself-and-you-are-the-easiest-person-richard-p-feynman-9-53-68.jpg';
+const imgSrc = 'https://www.google.co.il/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
 
 class Query extends Component {
   state = {
     query: '',
   }
-
-  setQuery = ({ target: { value } })=> this.setState({ query: value })
-  
-  render() {
-    return (
-      <div className='Query'>
-        <img src={imgSrc} alt='what is this tomfoolery?'/>
-        <br/>
-        <input value={this.state.query} onChange={this.setQuery}/>
-        <br/>
-        <button onClick={()=> this.props.onSearch(this.state.query)}
-                disabled={!this.state.query.length}>
-          Search
-        </button>
-      </div>
-    );
-  }
+    
+    setQuery = ({ target: { value } })=> this.setState({ query: value })
+    
+    render() {
+	return (
+	    <div className='Query'>
+		<img className='googleImg' src={imgSrc} alt='what is this tomfoolery?'/>
+		<br/>
+		<input value={this.state.query} onChange={this.setQuery}/>
+		<br/>
+		<button onClick={()=> this.props.onSearch(this.state.query)}
+            disabled={!this.state.query.length}>
+		Search
+            </button>
+		<button
+	    onClick={()=> this.props.onSearch(this.state.query)}
+	    disabled={!this.state.query.length}>I'm feeling lucky</button>
+		</div>
+	);
+    }
 }
 
 export default Query;
