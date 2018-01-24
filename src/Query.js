7f0708886d6563e7243a78dbc802792f67cfb8e5
nik frank <nikbfrank@gmail.com>
2018-01-24 14:32:43 +0200

    ugly autocomplete

--- src/Query.js
+++ src/Query.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import './Query.css';
 
-
 const imgSrc = 'https://www.google.co.il/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+
+const mockAutos = [
+  'nik', 'nox', 'lax', 'lex'
+];
 
 class Query extends Component {
   state = {
     query: '',
+    autos: [],
   }
   
-  setQuery = ({ target: { value } })=> this.setState({ query: value })
+  setQueryFromInput = ({ target: { value } })=> {
+    this.setState({ query: value });
+    
+    value ?
+    this.setState({ autos: mockAutos.filter(a => a.includes(value)) }) :
+    this.setState({ autos: [] });
+  }
+  
+  makeQuery = value => {
+    this.setState({ query: value });
+    this.props.onSearch( value );
+  }
   
   render() {
+    const { autos } = this.state;
+    
     return (
       <div className='Query'>
 	<img className='googleImg' src={imgSrc} alt='what is this tomfoolery?'/>
-	<br/>
-	<input value={this.state.query} onChange={this.setQuery}/>
-	<br/>
+	<div>
+	  <input value={this.state.query} autoFocus onChange={this.setQueryFromInput}/>
+          {
+            !autos.length ? null : (
+              <ul>
+                {
+                  autos.map((auto, i)=> (
+                    <li key={i} onClick={()=> this.makeQuery(auto)}>
+                      {auto}
+                    </li>
+                  ) )
+                }
+              </ul>
+            )
+          }
+	</div>
 	<button onClick={()=> this.props.onSearch(this.state.query)}
                 disabled={!this.state.query.length}>
 	  Search
